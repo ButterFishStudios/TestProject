717a161c2ae78c66d8460ad5bfb6098b1af7d077
Cooper <cjmahring@gmail.com>
2015-03-07 14:36:14 -0600

    added player movement and wall movement

---
+++ Test Project/Assets/playerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	bool onGround = true;
+	public float maxSpeed = 200f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("w")&&onGround){//jump
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+		}
+		if (Input.GetKey ("w") && !onGround) {//long jump
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0.05f), ForceMode2D.Impulse);
+		}
+		if(Input.GetKey ("a")){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.2f, 0), ForceMode2D.Impulse);
+		}
+		if(Input.GetKey ("d")){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0.2f, 0), ForceMode2D.Impulse);
+		}
+
+		//make sure the player isn't going too fast
+		/*if(GetComponent<Rigidbody2D>().velocity.magnitude > maxSpeed)
+		{
+			GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity.normalized * maxSpeed;
+		}*/
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.name == "Wall") {
+			//GameOver
+			Debug.Log ("Game Over");
+			Application.LoadLevel(0);
+		}
+		if (coll.gameObject.tag == "ground") {
+			onGround = true;
+		}
+		
+	}
+	
+	void OnCollisionExit2D(Collision2D coll){
+		if (coll.gameObject.tag == "ground") {
+			onGround = false;
+		}
+	}
+}
---
+++ Test Project/Assets/playerPhysics.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerPhysics : MonoBehaviour {
+
+	public bool onGround = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("w")&&onGround){//jump
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.name == "Wall") {
+			//GameOver
+			Debug.Log ("Game Over");
+			Application.LoadLevel(0);
+		}
+		if (coll.gameObject.name == "Tile") {
+			onGround = true;
+		}
+	
+	}
+
+	void OnCollisionExit2D(Collision2D coll){
+		if (coll.gameObject.name == "Tile") {
+			onGround = false;
+		}
+	}
+}
